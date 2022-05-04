52dab648e542d85e12fdb066167c1caa742fd110
Christophe <grebert62231@gmail.com>
2022-05-04 10:20:34 +0200

    first commit

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\ArticleRepository;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/home", name="app_home")
+     */
+    public function index(): Response
+    {
+        return $this->render('home/index.html.twig', [
+            'controller_name' => 'HomeController',
+        ]);
+    }
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(ArticleRepository $repoArticle): Response
+    {
+        $articles = $repoArticle->findAll();
+        return $this->render('home/index.html.twig', [
+            "articles" => $articles
+        ]);
+    }
+    /**
+     * @Route("/show/{id}", name="show")
+     */
+    // public function show(ArticleRepository $repoArticle, $id): Response
+    // {
+    //     $article = $repoArticle->find($id);
+    //     if (!$article) {
+    //         $this->redirectToRoute("home");
+    //     }
+    //     return $this->render('show/index.html.twig', [
+    //         "article" => $article
+    //     ]);
+    // }
+    /**
+     * @Route("/show/{id}", name="show")
+     */
+    public function show(ArticleRepository $repoArticle, $id): Response
+    {
+        $article = $repoArticle->find($id);
+        if (!$article) {
+            $this->redirectToRoute("home");
+        }
+        return $this->render('show/index.html.twig', [
+            "article" => $article
+        ]);
+    }
+
+}
---
+++ src/Entity/Category.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text", nullable=true)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $image;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Article::class, mappedBy="category")
+     */
+    private $yes;
+
+    public function __construct()
+    {
+        $this->yes = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(?string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Article>
+     */
+    public function getYes(): Collection
+    {
+        return $this->yes;
+    }
+
+    public function addYe(Article $ye): self
+    {
+        if (!$this->yes->contains($ye)) {
+            $this->yes[] = $ye;
+            $ye->addCategory($this);
+        }
+
+        return $this;
+    }
+
+    public function removeYe(Article $ye): self
+    {
+        if ($this->yes->removeElement($ye)) {
+            $ye->removeCategory($this);
+        }
+
+        return $this;
+    }
+}
